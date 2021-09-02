0c9b009354c6e49e3f2beaac414939c414bf3c29
Robert Helmer <rhelmer@rhelmer.org>
2021-09-02 00:54:25 -0700

    port rally-core-addon glean.js un/enrollment and demographic pings to cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,25 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
+import Glean from "@mozilla/glean/webext";
+import PingEncryptionPlugin from "@mozilla/glean/webext/plugins/encryption";
+
+import * as rallyMetrics from "../public/generated/rally";
+import * as userMetrics from "../public/generated/user";
+import * as enrollmentMetrics from "../public/generated/enrollment";
+import * as unenrollmentMetrics from "../public/generated/unenrollment";
+import * as rallyPings from "../public/generated/pings";
+
 import { studies } from "./studies";
 import { v4 as uuidv4 } from "uuid";
+
+const GLEAN_ENCRYPTION_JWK = {
+  "crv": "P-256",
+  "kid": "rally-core",
+  "kty": "EC",
+  "x": "m7Gi2YD8DgPg3zxora5iwf0DFL0JFIhjoD2BRLpg7kI",
+  "y": "zo35XIQME7Ct01uHK_LrMi5pZCuYDMhv8MUsSu7Eq08",
+};
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
@@ -77,7 +94,6 @@ exports.addRallyUserToFirestore = functions.auth.user().onCreate(
       return;
     }
 
-
     const newRallyId = uuidv4();
     const extensionUserDoc = { rallyId: newRallyId };
     admin
@@ -139,6 +155,239 @@ exports.deleteRallyUser = functions.auth.user().onDelete(
     return true;
   }
 );
+
+async function getRallyId(userId: string) {
+  const extensionUserRef = await admin
+    .firestore()
+    .collection("extensionUsers")
+    .doc(userId)
+    .get();
+
+  const extensionUserDoc = extensionUserRef.data();
+  if (!extensionUserDoc) {
+    throw new Error(`No extension user doc for user ID ${userId}`);
+  }
+
+  if (!extensionUserDoc.rallyId) {
+    throw new Error(`No rallyId field in extension user doc for user ID ${userId}`);
+  }
+
+  const rallyId = extensionUserDoc.rallyId;
+  return rallyId;
+}
+
+exports.handleUserChanges = functions.firestore
+  .document('users/{userID}')
+  .onWrite(async (change, context) => {
+    const userId = context.params.userId;
+    // Get an object with the current document value.
+    // If the document does not exist, it has been deleted.
+    const document = change.after.exists ? change.after.data() : null;
+
+    // Get the old document, to compare the enrollment state.
+    // const oldDocument = change.before.data();
+
+    if (!document) {
+      throw new Error(`No user document for user ID ${userId}`);
+    }
+    if (!document.enrolled) {
+      throw new Error(`No enrolled field in user document for user ID ${userId}`)
+    }
+
+    if (!document.demographicsData) {
+      throw new Error(`No demographics field in user document for user ID ${userId}`);
+    }
+
+    // TODO if demographics changed
+    await sendDemographicsPing(document.demographicsData);
+
+    const rallyId = await getRallyId(userId);
+
+    if (document.enrolled === "true") {
+      await sendPlatformEnrollmentPing(rallyId);
+    } else {
+      await sendPlatformDeletionPing(rallyId);
+    }
+  });
+
+exports.handleUserStudyChanges = functions.firestore
+  .document('users/{userID}/studies/{studyId}')
+  .onWrite(async (change, context) => {
+    const userId = context.params.userId;
+    const studyId = context.params.studyId;
+    // Get an object with the current document value.
+    // If the document does not exist, it has been deleted.
+    const document = change.after.exists ? change.after.data() : null;
+
+    // Get the old document, to compare the enrollment state.
+    // const oldDocument = change.before.data();
+
+    if (!document) {
+      throw new Error(`No userStudy document for user ID ${userId} with study ID ${studyId}`);
+    }
+
+    if (!document.studyId) {
+      throw new Error(`User ID ${userId} missing study ID in `)
+    }
+
+    if (!document.enrolled) {
+      throw new Error(`No enrolled field in user studies document for user ID ${userId} with study ID ${studyId}`)
+    }
+
+    const rallyId = await getRallyId(userId);
+
+    if (document.enrolled === true) {
+      await sendStudyEnrollmentPing(rallyId, studyId)
+    } else if (document.enrolled === false) {
+      await sendStudyDeletionPing(rallyId, studyId);
+    } else {
+      throw new Error(`document.enrolled not boolean for user ID ${userId} with study ID ${studyId}`);
+    }
+  });
+
+async function sendPlatformEnrollmentPing(rallyId: string) {
+  Glean.initialize("rally-core", true, {
+    appDisplayVersion: "TODO-rally-firestore-server",
+    plugins: [
+      new PingEncryptionPlugin(GLEAN_ENCRYPTION_JWK)
+    ]
+  });
+
+  rallyMetrics.id.set(rallyId);
+
+  Glean.setUploadEnabled(true);
+
+  rallyMetrics.id.set(rallyId);
+
+  rallyPings.enrollment.submit();
+
+  // Wait for Glean to finish sending pending pings.
+  await Glean.shutdown();
+}
+
+async function sendPlatformDeletionPing(rallyId: string) {
+  Glean.initialize("rally-core", true, {
+    appDisplayVersion: "TODO-rally-firestore-server",
+    plugins: [
+      new PingEncryptionPlugin(GLEAN_ENCRYPTION_JWK)
+    ]
+  });
+
+  rallyMetrics.id.set(rallyId);
+
+  // Flip upload enabled to disabled: this will trigger a
+  // deletion-request.
+  Glean.setUploadEnabled(false);
+
+  // Wait for Glean to finish sending pending pings.
+  await Glean.shutdown();
+}
+
+async function sendStudyEnrollmentPing(rallyId: string, studyId: string) {
+  Glean.initialize("rally-core", true, {
+    appDisplayVersion: "TODO-rally-firestore-server",
+    plugins: [
+      new PingEncryptionPlugin(GLEAN_ENCRYPTION_JWK)
+    ]
+  });
+
+  Glean.setUploadEnabled(true);
+
+  rallyMetrics.id.set(rallyId);
+
+  enrollmentMetrics.studyId.set(studyId);
+  rallyPings.studyEnrollment.submit();
+
+  // Wait for Glean to finish sending pending pings.
+  await Glean.shutdown();
+}
+
+async function sendStudyDeletionPing(rallyId: string, studyId: string) {
+  Glean.initialize("rally-core", true, {
+    appDisplayVersion: "TODO-rally-firestore-server",
+    plugins: [
+      new PingEncryptionPlugin(GLEAN_ENCRYPTION_JWK)
+    ]
+  });
+
+  Glean.setUploadEnabled(true);
+
+  unenrollmentMetrics.studyId.set(studyId);
+  rallyPings.studyUnenrollment.submit();
+
+  // Wait for Glean to finish sending pending pings.
+  await Glean.shutdown();
+}
+
+/**
+ * Sends a demographic-survey ping with Glean.js.
+ *
+ * @param {Object} data
+ *        A JSON-serializable object containing the demographics
+ *        information submitted by the user..
+ */
+async function sendDemographicsPing(data: { [x: string]: any; }) {
+  Glean.initialize("rally-core", true, {
+    appDisplayVersion: "TODO-rally-firestore-server",
+    plugins: [
+      new PingEncryptionPlugin(GLEAN_ENCRYPTION_JWK)
+    ]
+  });
+
+  Glean.setUploadEnabled(true);
+
+  // The schema for the non-glean collection is hard to change.
+  // In order for us to not change it, we transform the provided
+  // fields in a way that's expected by Glean.
+
+  if ("age" in data) {
+    userMetrics.age[`band_${data["age"]}`].set(true);
+  }
+
+  if ("gender" in data) {
+    userMetrics.gender[data["gender"]].set(true);
+  }
+
+  if ("hispanicLatinxSpanishOrigin" in data) {
+    const label = (data["hispanicLatinxSpanishOrigin"] === "other")
+      ? "other" : "hispanicLatinxSpanish";
+    userMetrics.origin[label].set(true);
+  }
+
+  if ("race" in data) {
+    for (const raceLabel of data["race"]) {
+      const label = (raceLabel === "american_indian_or_alaska_native")
+        ? "am_indian_or_alaska_native" : raceLabel;
+      userMetrics.races[label].set(true);
+    }
+  }
+
+  if ("school" in data) {
+    const KEY_FIXUP = {
+      "high_school_graduate_or_equivalent": "high_school_grad_or_eq",
+      "some_college_but_no_degree_or_in_progress": "college_degree_in_progress",
+    };
+
+    const originalLabel = data["school"];
+    const label = (originalLabel in KEY_FIXUP)
+      //@ts-ignore FIXME
+      ? KEY_FIXUP[originalLabel] : originalLabel;
+    userMetrics.school[label].set(true);
+  }
+
+  if ("exactIncome" in data) {
+    userMetrics.exactIncome.set(data['exactIncome']);
+  }
+
+  if ("zipcode" in data) {
+    userMetrics.zipcode.set(data["zipcode"]);
+  }
+
+  rallyPings.demographics.submit();
+
+  // Wait for Glean to finish sending pending pings.
+  await Glean.shutdown();
+}
 
 /**
  *
